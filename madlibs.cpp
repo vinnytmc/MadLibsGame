12d49af6804b3c8c2574d3e34788ff76b82dd4d4
vinnytmv <valopez@mail.csuchico.edu>
2018-09-12 18:12:08 -0700

    my midlimb game

--- madlibs.cpp
+++ madlibs.cpp
@@ -2,8 +2,28 @@
 #include <iostream>
 #include <string>
 
+
 int main()
 {
+std::string  name1;
+int num1;
+std::string  verb1;
+std::string  noun1;
+std::string  noun2;
+
+std::cout<<"please enter name:\n";
+std::cin>>name1;
+std::cout<<"please enter number:\n";
+std::cin>>num1;
+std::cout<<"please enter verb:\n";
+std::cin>>verb1;
+std::cout<<"please enter noun:\n";
+std::cin>>noun1;
+std::cout<<"please enter noun:\n";
+std::cin>>noun2;
+std::cout<<name1<<" is turning "<<num1<<" years old this friday\n";
+std::cout<<"He/she wants to do somthing "<<verb1<<" for their birthday!\n";
+std::cout<<name1<<" likes "<<noun1<<" and "<<noun2<<" so whatever he/she does must include this!\n";
 
   return 0;
 }
